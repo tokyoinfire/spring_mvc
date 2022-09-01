f9f566ece22e1b3f63a12bbe0c0032e48c2c5003
sveklenko15@gmail.com <0995658527Satan>
2022-09-01 13:59:00 +0300

    adding some things

--- src/main/java/com/veklenko/spring/mvc/MyController.java
+++ src/main/java/com/veklenko/spring/mvc/MyController.java
@@ -1,7 +1,11 @@
 package com.veklenko.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -16,8 +20,30 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmployeeDetails() {
+//        return "show-emp-details-view";
+//    }
+
+
+//    @RequestMapping("/showDetails")
+//    public String showEmployeeDetails(HttpServletRequest request, Model model) {
+//
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr." + empName;
+//        model.addAttribute("nameAttribute", empName);
+//
+//        return "show-emp-details-view";
+//    }
+
+
     @RequestMapping("/showDetails")
-    public String showEmployeeDetails() {
+    public String showEmployeeDetails(@RequestParam("employeeName") String empName
+            , Model model) {
+
+        empName = "Mr." + empName;
+        model.addAttribute("nameAttribute", empName);
+
         return "show-emp-details-view";
     }
 }
--- src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -4,6 +4,8 @@
     <title>Title</title>
 </head>
 <body>
-<h2>Dear ${param.employeeName}, you are WELCOME!</h2>
+<%--<h2>Dear ${param.employeeName}, you are WELCOME!</h2>--%>
+
+<h2>Your name is ${nameAttribute}<h2/>
 </body>
 </html>
